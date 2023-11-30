812a4d3ce1c6cba92d6909db389f12cc289a2e96
pedrohqa <130668972+pedrohqa@users.noreply.github.com>
2023-11-30 11:08:16 -0300

    p

--- templates/header.php
+++ templates/header.php
@@ -3,24 +3,30 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Blog Cinema</title>
-  <!-- ESTILOS DO PROJETO -->
-  <link rel="stylesheet" href="<?= $BASE_URL ?>/styles.css">
+  <title>AconCheguei</title>
   <!-- GOOGLE FONTS -->
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
 </head>
 <body>
   <header>
-    <a href="<?= $BASE_URL ?>" id="logo">
-      <img src="<?= $BASE_URL ?>/img/logocine.png" alt="Blog Codar">
-    </a>
-    <nav>
-      <ul id="navbar">
-        <li><a href="<?= $BASE_URL ?>index.php" class="nav-link">Home</a></li>
-        <li><a href="<?= $BASE_URL ?>categorias1.php" class="nav-link">Categorias</a></li>
-        <li><a href="<?= $BASE_URL ?>sobre.php" class="nav-link">Sobre</a></li>
-        <li><a href="<?= $BASE_URL ?>contato.php" class="nav-link">Contato</a></li>
-      </ul>
-    </nav>
+  <div class="navbar dropdown">
+    <img src="../sprint3/img/Logo (2).png" alt="Logo">
+
+    <div class="logo">
+        <img src="../sprint3/img/Logo (3).png">
+    </div>
+
+    <div class="login">
+        <img src="../sprint3/img/login.png" alt="Login">
+    </div>
+
+    <div class="subnav">
+        <a href="#item1"><img src="../sprint3/img/piscinaslogo.png"></a>
+        <a href="#item2"><img src="../sprint3/img/chale.png"></a>
+        <a href="#item3"><img src="../sprint3/img/hoteis.png"></a>
+        <a href="#item4"><img src="../sprint3/img/montanhas.png"></a>
+        <a href="#item5"><img src="../sprint3/img/beiramar.png"></a>
+    </div>
+</div>
   </header>
