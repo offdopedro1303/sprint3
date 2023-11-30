fb1cbee1c00a35411358ecd60d3c215da78aa04a
pedrohqa <130668972+pedrohqa@users.noreply.github.com>
2023-11-30 13:33:19 -0300

    pedro

---
+++ templates/footer.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <footer>
+        
+    </footer>
+  </body>
+</html>
--- templates/header.php
+++ templates/header.php
@@ -1,29 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>AconCheguei</title>
-  <!-- GOOGLE FONTS -->
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
-</head>
-<body>
-  <header>
-  <div class="navbar dropdown">
-    <img src="../sprint3/img/Logo (2).png" alt="Logo">
-    <div class="logo">
-        <img src="../sprint3/img/Logo (3).png">
+<html>
+  <body>
+    <header>
+    <div class="navbar dropdown">
+      <img src="../sprint3/img/Logo (2).png" alt="Logo">
+      <div class="logo">
+          <img src="../sprint3/img/Logo (3).png">
+      </div>
+      <div class="login">
+          <img src="../sprint3/img/login.png" alt="Login">
+      </div>
+      <div class="subnav">
+          <a href="../sprint3/piscinas/piscinas.php"><img src="img/piscinaslogo.png"></a>
+          <a href="../sprint3/beiramar/beiramar.php"><img src="../sprint3/img/beiramar.png"></a>
+          <a href="../sprint3/hoteis/hoteis.php"><img src="../sprint3/img/hoteis.png"></a>
+          <a href="../sprint3/chales/chale.php"><img src="../sprint3/img/chale.png"></a>
+          <a href="../sprint3/montanhas/montanhas.php"><img src="../sprint3/img/montanhas.png"></a>
+      </div>
     </div>
-    <div class="login">
-        <img src="../sprint3/img/login.png" alt="Login">
-    </div>
-    <div class="subnav">
-        <a href="../sprint3/piscinas/piscinas.php"><img src="../sprint3/img/piscinaslogo.png"></a>
-        <a href="../sprint3/beiramar/beiramar.php"><img src="../sprint3/img/beiramar.png"></a>
-        <a href="../sprint3/hoteis/hoteis.php"><img src="../sprint3/img/hoteis.png"></a>
-        <a href="../sprint3/chales/chale.php"><img src="../sprint3/img/chale.png"></a>
-        <a href="../sprint3/montanhas/montanhas.php"><img src="../sprint3/img/montanhas.png"></a>
-    </div>
-  </div>
-  </header>
+    </header>
+  </body>
+</html>
