2c6cd7bdf600d0ee0a34ce1b4b496fe6368c9015
pedrohqa <130668972+pedrohqa@users.noreply.github.com>
2023-12-01 11:26:01 -0300

    p

--- templates/header.php
+++ templates/header.php
@@ -2,15 +2,17 @@
 <html>
   <body>
     <header>
-    <div class="navbar dropdown">
-      <img src="../sprint3/img/Logo (2).png" alt="Logo">
-      <div class="logo">
-          <img src="../sprint3/img/Logo (3).png">
-      </div>
-      <div class="login">
-          <img src="../sprint3/img/login.png" alt="Login">
-      </div>
+    <div class="navbar">
       <div class="subnav">
+        <img src="../sprint3/img/Logo (2).png" alt="Logo">
+        <div class="logo">
+            <img src="../sprint3/img/Logo (3).png">
+        </div>
+        <div class="login">
+            <img src="../sprint3/img/login.png" alt="Login">
+        </div>
+      </div>
+      <div class="subnav2">
           <a href="../sprint3/piscinas.php"><img src="img/piscinaslogo.png"></a>
           <a href="../sprint3/beiramar.php"><img src="../sprint3/img/beiramar.png"></a>
           <a href="../sprint3/hoteis.php"><img src="../sprint3/img/hoteis.png"></a>
@@ -18,71 +20,73 @@
           <a href="../sprint3/montanhas.php"><img src="../sprint3/img/montanhas.png"></a>
       </div>
     </div>
+    <br>
+    <br>
     </header>
     <style>
       .navbar {
-    background-color: #544013;
-    overflow: hidden;
-display: block;
-}
+        background-color: #544013;
+        }
 
-.navbar img {
-    float: left;
-    margin-top: 5px;
-    margin-left: 10px;
-    height: 40px; /* ajuste conforme necessário */
-}
+    .navbar img {
+        float: left;
+        margin-top: 5px;
+        margin-left: 5%;
+        height: 40px; /* ajuste conforme necessário */
+    }
 
-.navbar .logo {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 50px; /* ajuste conforme necessário */
-    color: white;
-    text-decoration: none;
-    font-size: 1.5em;
-}
+    .navbar .logo {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 50px; /* ajuste conforme necessário */
+        color: white;
+        text-decoration: none;
+        font-size: 1.5em;
+        margin-right: 12%;
+        }
 
-.navbar .logo img {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 20px; /* ajuste conforme necessário */
+    .navbar .logo img {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 20px; /* ajuste conforme necessário */
 
-}
-.navbar .logo img {
-    display: flex;
-    justify-content:right;
-    align-items:baseline;
-    height: 20px; /* ajuste conforme necessário */
-}
-.navbar .login {
-    float: right;
-    display: flex; /* Adicionado para alinhar os itens na mesma linha */
-    align-items: center; /* Alinha verticalmente com a logo */
-    margin-right: 1px;
-}
-.login img {
-    height: 30px; /* Ajuste conforme necessário */
-    margin-left: 10px; /* Ajuste conforme necessário */
-    margin-top: -50px;
-}
-.subnav {
-    display: none;
-    background-color: #F4F0D8;
-    overflow: hidden;
-}
-.subnav a {
-    float: left;
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-}
-.dropdown:hover .subnav {
-    display: block;
-}
+    }
+    .navbar .logo img {
+        display: flex;
+        justify-content:right;
+        align-items:baseline;
+        height: 20px; /* ajuste conforme necessário */
+    }
+    .navbar .login {
+        float: right;
+        display: flex; /* Adicionado para alinhar os itens na mesma linha */
+        align-items: center; /* Alinha verticalmente com a logo */
+        margin-right: 5%;
+    }
+    .login img {
+        height: 30px; /* Ajuste conforme necessário */
+        margin-left: 10px; /* Ajuste conforme necessário */
+        margin-top: -50px;
+    }
+    .subnav{
+      background-color: #544013;
+      position: fixed;
+      top: 0px;
+      height: auto;
+      width: 100%;
+      z-index: 999;
+    }
+    .subnav2 {
+      position: relative;
+        display: flex;
+        background-color: #FFBC49;
+        top: 50px;
+        justify-content: center;
+        gap: 10%; 
+        padding: 8px 14px;
+    }
     </style>
   </body>
 </html>
