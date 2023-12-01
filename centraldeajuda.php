ba124f227515fc7c1de41be0def3e747b1dec7f9
Natallyreis <130668435+Natallyreis@users.noreply.github.com>
2023-12-01 10:47:23 -0300

    commit

--- centraldeajuda.php
+++ centraldeajuda.php
@@ -9,14 +9,18 @@
             font-family: Arial, sans-serif;
             margin: 0;
             padding: 0;
-            background-color: #f4f4f4;
+            background-color: #f4f4f6;
         }
 
         header {
             background-color: #333;
-            color: #fff;
+            color: white;
             padding: 10px;
             text-align: center;
+        }
+
+        header h1 {
+            color: white;
         }
 
         section {
@@ -79,11 +83,55 @@
                 <p>Na página de login, clique em "Esqueceu a senha?" e siga os passos para a recuperação.</p>
             </li>
             <!-- Adicione mais perguntas frequentes conforme necessário -->
+            <strong>Como Reservar:</strong>
+                <p>Como encontrar e selecionar um hotel.
+                    <br>
+                    Processo de reserva passo a passo.
+                    <br>
+                    Políticas de pagamento e cancelamento.</p>
+            </li>
+            <strong>Informações sobre Hotéis:</strong>
+                <p>Descrição dos serviços e comodidades oferecidos.
+                    <br>
+                    Políticas específicas de cada hotel.
+                    <br>
+                    Avaliações e classificações de usuários.</p>
+            </li>
+            <strong>Gerenciar Reservas:</strong>
+                <p>Como visualizar e modificar reservas.
+                    <br>
+                    Adicionar serviços adicionais (estacionamento, café da manhã, etc.).
+                    <br>
+                    Políticas de alteração de reserva.</p>
+            </li>
+            <strong>Pagamentos e Faturas:</strong>
+                <p>Métodos de pagamento aceitos.
+                    <br>
+                    Informações sobre faturas e recibos.
+                    <br>
+                    Perguntas frequentes sobre cobranças.</p>
+            </li>
+            <strong>Segurança e Privacidade:</strong>
+                <p>Políticas de segurança de dados.
+                    <br>
+                    Como proteger a conta do usuário.
+                    <br>
+                    Informações sobre certificados de segurança.</p>
+            </li>
+            <strong>Contato e Suporte:</strong>
+                <p>Como entrar em contato com o suporte ao cliente.
+                    <br>
+                    Horários de atendimento.
+                    <br>
+                    Resolução de problemas comuns.</p>
+            </li>
+
+        
         </ul>
     </section>
 
     <footer>
-        &copy; 2023 aconCheguei. Todos os direitos reservados.
+        &copy; 2023 aconheguei. Todos os direitos reservados.
     </footer>
 
 </body>
