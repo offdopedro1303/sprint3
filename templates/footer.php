8a0d5b4fc9a77a0e94df99019d56b7c0946890a8
pedrohqa <130668972+pedrohqa@users.noreply.github.com>
2023-12-01 08:41:17 -0300

    p

--- templates/footer.php
+++ templates/footer.php
@@ -3,10 +3,42 @@
     <body>
         <div class="footer">
             <div class="pt1">
-                <h1></h1>
+                <h2>Atendimento</h2>
+                <p>Central de Ajuda</p>
+                <p>Cancelamento</p>
+                <p>Blog</p>
             </div>
-            <div class="pt2"></div>
-            <div class="pt3"></div>
+            <div class="pt2">
+                <h2>Aconcheguei</h2>
+                <p>Colaboradores
+                    AraraZoll </p>
+            </div>
+            <div class="pt3">
+                <h2>Contato</h2>
+                <p>email@email.com </p>
+                <p>(11)99999-9999</p>
+                <p>(11)0000-0000</p>
+            </div>
+        </div>
+        <br>
+        <br>
+        <div class="footer2">
+            <p>&copy; [2023] [Aconchequei]. Todos os direitos reservados.</p>
         </div>
     </body>
 </html>
+
+<style>
+    .footer{
+        display: flex;
+        justify-content: center;
+        gap: 25%;
+        color: rgba(84, 64, 19, 1);
+        
+    }
+    .footer2{
+        display: flex;
+        justify-content: center;
+     
+    }
+</style>
