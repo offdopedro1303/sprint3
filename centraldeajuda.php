ca5c6f2f14096503c72e7889160b7557808cdb64
Natallyreis <130668435+Natallyreis@users.noreply.github.com>
2023-12-01 08:44:47 -0300

    commit

---
+++ centraldeajuda.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Central de Ajuda</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
+
+        header {
+            background-color: #333;
+            color: #fff;
+            padding: 10px;
+            text-align: center;
+        }
+
+        section {
+            max-width: 800px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            color: #333;
+        }
+
+        h2 {
+            color: #555;
+        }
+
+        p {
+            line-height: 1.6;
+            color: #666;
+        }
+
+        ul {
+            list-style-type: none;
+            padding: 0;
+        }
+
+        li {
+            margin-bottom: 10px;
+        }
+
+        footer {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 10px;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+
+    <header>
+        <h1>Central de Ajuda</h1>
+    </header>
+
+    <section>
+        <h2>Perguntas Frequentes</h2>
+
+        <ul>
+            <li>
+                <strong>Como faço para criar uma conta?</strong>
+                <p>Para criar uma conta, clique no botão "Registrar" e siga as instruções.</p>
+            </li>
+            <li>
+                <strong>Esqueci minha senha, como recuperar?</strong>
+                <p>Na página de login, clique em "Esqueceu a senha?" e siga os passos para a recuperação.</p>
+            </li>
+            <!-- Adicione mais perguntas frequentes conforme necessário -->
+        </ul>
+    </section>
+
+    <footer>
+        &copy; 2023 aconCheguei. Todos os direitos reservados.
+    </footer>
+
+</body>
+</html>
