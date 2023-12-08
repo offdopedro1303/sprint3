78c3cad8938871d1394d58b3b3ca469658ac912e
Natallyreis <130668435+Natallyreis@users.noreply.github.com>
2023-12-08 14:10:20 -0300

    commit

--- be1.php
+++ be1.php
@@ -47,8 +47,8 @@ O apartamento na Guilhermina pode apresentar dois ou mais quartos, cada um cuida
     <button id="btnReservar">Reservar</button>
   </div>
 
-<div class="comentarios">
-    <div class="com1"><img src="img/comentario1.png"><img src="img/comentario2.png"></div>
+  <div class="comentarios">
+    <div class="com1"><img src="../sprint3/img/comentarios/comentario2.png"><img src="../sprint3/img/comentarios/comentario3.png"></div>
 </div>
 
 
---
+++ blog.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Blog</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+            color: #333;
+        }
+
+
+        section {
+            padding: 2em;
+        }
+
+        article {
+            background-color: #fff;
+            padding: 1em;
+            margin-bottom: 1em;
+            border-radius: 5px;
+        }
+
+    </style>
+</head>
+<body>
+
+<?php
+  include_once("templates/header.php");
+?>
+
+
+    <section>
+        <article>
+            <h2>Título do Artigo 1</h2>
+            <p>Conteúdo do artigo 1...</p>
+        </article>
+
+        <article>
+            <h2>Título do Artigo 2</h2>
+            <p>Conteúdo do artigo 2...</p>
+        </article>
+
+        <article>
+            <h2>Título do Artigo 2</h2>
+            <p>Conteúdo do artigo 2...</p>
+        </article>
+
+        <article>
+            <h2>Título do Artigo 2</h2>
+            <p>Conteúdo do artigo 2...</p>
+        </article>
+
+        <article>
+            <h2>Título do Artigo 2</h2>
+            <p>Conteúdo do artigo 2...</p>
+        </article>
+
+        <article>
+            <h2>Título do Artigo 2</h2>
+            <p>Conteúdo do artigo 2...</p>
+        </article>
+    </section>
+
+    <footer>
+            <?php
+            include_once("templates/footer.php");
+            ?>
+        </footer>
+
+</body>
+</html>
