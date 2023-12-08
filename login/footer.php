b19d2e8e8c06d00174e0f6fcdeb6db88f9c62cdc
pedrohqa <130668972+pedrohqa@users.noreply.github.com>
2023-12-08 11:38:44 -0300

    p

---
+++ login/footer.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <div class="footer">
+            <div class="pt1">
+                <h2>Atendimento</h2>
+                <a href="centraldeajuda.php"><p>Central de Ajuda</p></a>
+                <p>Blog</p>
+            </div>
+            <div class="pt2">
+                <h2>Aconcheguei</h2>
+                <p>Time Scrum</p>
+                    <p>AraraZoll</p>
+            </div>
+            <div class="pt3">
+                <h2>Contato</h2>
+                <p>acon_cheguei@hotel.com </p>
+                <p>(11)82763-0296</p>
+            </div>
+        </div>
+        <div class="footer2">
+            <p>&copy; [2023] [Aconchequei]. Todos os direitos reservados.</p>
+        </div>
+    </body>
+</html>
+
+<style>
+    .footer{
+        display: flex;
+        justify-content: center;
+        gap: 25%; 
+        background-color: wheat;
+        color: #544013;
+        text-align: center;
+    }
+    .footer2{
+        display: flex;
+        justify-content: center;
+        background-color: #544013;
+        color: wheat;
+    }
+    a{
+        text-decoration:  none;
+        color: #544013;
+    }
+</style>
