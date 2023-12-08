998b157edc5c1ceb6989ccb89551b6964bfb4fcf
Natallyreis <130668435+Natallyreis@users.noreply.github.com>
2023-12-08 09:48:16 -0300

    commit

---
+++ be1.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style2.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+ <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+<body>
+<?php
+  include_once("templates/header.php");
+?>
+
+<div class="container">
+<img src="img/beiramar/beiramar1.jpg" alt="Imagem 1">
+</div>
+
+<div class="texto">
+    <h1>Apê Praia Guilhermina<img src="img/estrela.png">   4,47 </h1>
+    <br>
+    <p>O apartamento na Praia Guilhermina, em Praia Grande, no litoral sul paulista, é um verdadeiro refúgio à beira-mar, oferecendo uma experiência única de vida costeira. Localizado em um dos destinos mais procurados da região, este aconchegante apartamento combina conforto, praticidade e uma vista deslumbrante para o oceano.
+
+Ao adentrar o apê, você será recebido por uma atmosfera leve e arejada, onde a luz natural ressalta os tons suaves da decoração. A sala de estar é decorada com mobiliário moderno e elegante, proporcionando um espaço convidativo para relaxar e socializar. Grandes janelas ou uma varanda ampla podem permitir que você desfrute da brisa do mar e contemple o pôr do sol sobre as águas tranquilas.
+
+A cozinha, integrada à sala de estar, é equipada com eletrodomésticos modernos e utensílios essenciais, tornando a preparação de refeições uma tarefa fácil e agradável. A área de refeições, seja uma mesa de jantar ou balcão de cozinha, oferece um ambiente confortável para desfrutar de suas refeições com vista para o oceano.
+
+O apartamento na Guilhermina pode apresentar dois ou mais quartos, cada um cuidadosamente decorado para proporcionar um refúgio tranquilo. O quarto principal pode oferecer uma cama confortável e espaço de armazenamento generoso, enquanto os quartos adicionais proporcionam acomodações aconchegantes para família ou amigos.</p>
+</div>
+<br>
+<br>
+<br>
+<br>
+<div class="ben"style="text-align: center;">
+    <label for="datepickerChegada">Data de Chegada:</label>
+    <input type="text" id="datepickerChegada" class="datepicker" readonly>
+
+    <label for="datepickerSaida">Data de Saída:</label>
+    <input type="text" id="datepickerSaida" class="datepicker" readonly>
+
+    <label for="quantidadeHospedes">Quantidade de Hóspedes:</label>
+    <input type="number" id="quantidadeHospedes" min="1" value="1">
+
+    <button id="btnReservar">Reservar</button>
+  </div>
+
+<div class="comentarios">
+    <div class="com1"><img src="img/comentario1.png"><img src="img/comentario2.png"></div>
+</div>
+
+
+
+<br>
+  <div class="card-container">
+    <div class="card">
+        <div class="card-body">
+            <img src="img/beliche.webp" alt="Imagem 3">
+            <h5 class="card-title">Casa em Suzano</h5>
+            <p class="card-text"><img src="img/estrela.PNG">4,29 <a>R$200,00</a></p>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-body">
+            <img src="img/cozinha.webp" alt="Imagem 3">
+            <h5 class="card-title">Chácara em Caraguatatininga</h5>
+            <p class="card-text"><img src="img/estrela.PNG">4,57 <a>R$419,00</a></p>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-body">
+            <img src="img/espaco.webp" alt="Imagem 3">
+            <h5 class="card-title">Rio de Janeiro House</h5>
+            <p class="card-text"><img src="img/estrela.PNG">3,87 <a>R$316,00</a></p>
+        </div>
+    </div>
+  </div>
+<BR>
+<BR>
+<div class="card-container" id="cardContainer">
+
+    <!-- Cartões adicionados pelo botão MOSTRAR MAIS serão inseridos aqui -->
+</div>
+<div class="mostrarmais-container">
+    <button class="mostrarmais" onclick="showMoreCards()">MOSTRAR MAIS</button>
+</div>
+        <footer>
+            <?php
+            include_once("templates/footer.php");
+            ?>
+        </footer>
+        <script>
+    // script.js
+    let cardContainer = document.getElementById("cardContainer");
+
+function showMoreCards() {
+    // Informações para os novos cards
+    const newCardInfo = [
+        { name: "Apê José Menino", rating: "4,50", price: "R$190,00" },
+        { name: "Resort Maragogi", rating: "5,00", price: "R$1369,00" },
+        { name: "Apê no Guaruja", rating: "4,82", price: "R$299,00" }
+    ];
+
+    // Create new card elements and append them to the card container
+    for (let i = 0; i < newCardInfo.length; i++) {
+        let newCard = document.createElement("div");
+        newCard.classList.add("card");
+        newCard.innerHTML = `
+            <div class="card-body">
+            <img src="../sprint3/img/beiramar/beiramar${i+7}.jpg" alt="Nova Imagem ${i+1}">
+                <h5 class="card-title">${newCardInfo[i].name}</h5>
+                <p class="card-text"><img src="img/estrela.PNG">${newCardInfo[i].rating} <a>${newCardInfo[i].price}</a></p>
+            </div>
+            <br>
+        `;
+        cardContainer.appendChild(newCard);
+    }
+
+    // Ocultar o botão MOSTRAR MAIS
+    document.querySelector(".mostrarmais-container").style.display = "none";
+}
+</script>
+        <footer>
+            <?php
+            include_once("templates/footer.php");
+            ?>
+        </footer>
+</body>
+<script>
+    $(function() {
+      $("#datepickerChegada").datepicker({
+        dateFormat: 'dd/mm/yy',
+        dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+        dayNamesMin: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+        monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+        monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+        nextText: 'Próximo',
+        prevText: 'Anterior',
+      });
+
+      $("#datepickerSaida").datepicker({
+        dateFormat: 'dd/mm/yy',
+        dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+        dayNamesMin: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+        monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+        monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+        nextText: 'Próximo',
+        prevText: 'Anterior',
+      });
+
+      $("#btnReservar").on("click", function() {
+        const chegadaSelecionada = $("#datepickerChegada").datepicker("getDate");
+        const saidaSelecionada = $("#datepickerSaida").datepicker("getDate");
+        const quantidadeHospedes = $("#quantidadeHospedes").val();
+
+        const chegadaFormatada = $.datepicker.formatDate('mm/dd/yy', chegadaSelecionada);
+        const saidaFormatada = $.datepicker.formatDate('mm/dd/yy', saidaSelecionada);
+
+        localStorage.setItem("chegadaReserva", JSON.stringify({ data: chegadaFormatada, valor: 100 }));
+        localStorage.setItem("saidaReserva", JSON.stringify({ data: saidaFormatada, valor: 100 }));
+        localStorage.setItem("quantidadeHospedes", quantidadeHospedes);
+
+        window.location.href = "pagamento.php";
+      });
+    });
+  </script>
+</html>
--- be2.php
+++ be2.php
@@ -2,22 +2,164 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="style2.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    
 </head>
-<body>
 
+ <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+<body>
 <?php
-  include_once("../templates/header.php");
+  include_once("templates/header.php");
 ?>
 
+<div class="container">
+<img src="img/saolourenco.webp" alt="Imagem 1">
+</div>
+
+<div class="texto">
+    <h1>Chácara Encantadora em São Lourenço<img src="img/estrela.png">   4,47 </h1>
+    <br>
+    <p>Bem-vindo à Chácara Serenidade, uma propriedade encantadora disponível para aluguel por temporada em São Lourenço. Rodeada pela exuberante natureza local, esta chácara oferece tranquilidade e conforto para momentos de descanso. Com uma espaçosa área verde, piscina privativa e jardins paisagísticos, é o local ideal para atividades ao ar livre e piqueniques. A casa principal, aconchegante e decorada com elegância, possui varandas com vistas panorâmicas. A chácara ainda oferece um espaço gourmet com churrasqueira, atividades recreativas e está estrategicamente localizada para fácil acesso às atrações locais. Com capacidade para até 10 pessoas, é perfeita para famílias, grupos de amigos ou retiros. Entre em contato para informações sobre reservas e disponibilidade. A Chácara Serenidade promete criar memórias inesquecíveis em meio à serenidade da natureza. Reserve agora e vivencie essa experiência única!</p>
+</div>
+<br>
+<br>
+<br>
+<br>
+<div class="ben"style="text-align: center;">
+    <label for="datepickerChegada">Data de Chegada:</label>
+    <input type="text" id="datepickerChegada" class="datepicker" readonly>
+
+    <label for="datepickerSaida">Data de Saída:</label>
+    <input type="text" id="datepickerSaida" class="datepicker" readonly>
+
+    <label for="quantidadeHospedes">Quantidade de Hóspedes:</label>
+    <input type="number" id="quantidadeHospedes" min="1" value="1">
+
+    <button id="btnReservar">Reservar</button>
+  </div>
+
+<div class="comentarios">
+    <div class="com1"><img src="img/comentario1.png"><img src="img/comentario2.png"></div>
+</div>
 
 
+
+<br>
+  <div class="card-container">
+    <div class="card">
+        <div class="card-body">
+            <img src="img/beliche.webp" alt="Imagem 3">
+            <h5 class="card-title">Casa em Suzano</h5>
+            <p class="card-text"><img src="img/estrela.PNG">4,29 <a>R$200,00</a></p>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-body">
+            <img src="img/cozinha.webp" alt="Imagem 3">
+            <h5 class="card-title">Chácara em Caraguatatininga</h5>
+            <p class="card-text"><img src="img/estrela.PNG">4,57 <a>R$419,00</a></p>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-body">
+            <img src="img/espaco.webp" alt="Imagem 3">
+            <h5 class="card-title">Rio de Janeiro House</h5>
+            <p class="card-text"><img src="img/estrela.PNG">3,87 <a>R$316,00</a></p>
+        </div>
+    </div>
+  </div>
+<BR>
+<BR>
+<div class="card-container" id="cardContainer">
+
+    <!-- Cartões adicionados pelo botão MOSTRAR MAIS serão inseridos aqui -->
+</div>
+<div class="mostrarmais-container">
+    <button class="mostrarmais" onclick="showMoreCards()">MOSTRAR MAIS</button>
+</div>
         <footer>
-            <p>&copy; 2023 Nome da Sua Empresa. Todos os direitos reservados.</p>
+            <?php
+            include_once("templates/footer.php");
+            ?>
         </footer>
-        
+        <script>
+    // script.js
+    let cardContainer = document.getElementById("cardContainer");
+
+function showMoreCards() {
+    // Informações para os novos cards
+    const newCardInfo = [
+        { name: "Casa Tropical", rating: "4,50", price: "R$190,00" },
+        { name: "Casa Vila Tramontano", rating: "5,00", price: "R$369,00" },
+        { name: "Casa em Cotia", rating: "4,82", price: "R$299,00" }
+    ];
+
+    // Create new card elements and append them to the card container
+    for (let i = 0; i < newCardInfo.length; i++) {
+        let newCard = document.createElement("div");
+        newCard.classList.add("card");
+        newCard.innerHTML = `
+            <div class="card-body">
+                <img src="img/piscinas${i+1}.webp" alt="Nova Imagem ${i+1}">
+                <h5 class="card-title">${newCardInfo[i].name}</h5>
+                <p class="card-text"><img src="img/estrela.PNG">${newCardInfo[i].rating} <a>${newCardInfo[i].price}</a></p>
+            </div>
+            <br>
+        `;
+        cardContainer.appendChild(newCard);
+    }
+
+    // Ocultar o botão MOSTRAR MAIS
+    document.querySelector(".mostrarmais-container").style.display = "none";
+}
+</script>
+        <footer>
+            <?php
+            include_once("templates/footer.php");
+            ?>
+        </footer>
 </body>
+<script>
+    $(function() {
+      $("#datepickerChegada").datepicker({
+        dateFormat: 'dd/mm/yy',
+        dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+        dayNamesMin: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+        monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+        monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+        nextText: 'Próximo',
+        prevText: 'Anterior',
+      });
+
+      $("#datepickerSaida").datepicker({
+        dateFormat: 'dd/mm/yy',
+        dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+        dayNamesMin: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+        monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+        monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+        nextText: 'Próximo',
+        prevText: 'Anterior',
+      });
+
+      $("#btnReservar").on("click", function() {
+        const chegadaSelecionada = $("#datepickerChegada").datepicker("getDate");
+        const saidaSelecionada = $("#datepickerSaida").datepicker("getDate");
+        const quantidadeHospedes = $("#quantidadeHospedes").val();
+
+        const chegadaFormatada = $.datepicker.formatDate('mm/dd/yy', chegadaSelecionada);
+        const saidaFormatada = $.datepicker.formatDate('mm/dd/yy', saidaSelecionada);
+
+        localStorage.setItem("chegadaReserva", JSON.stringify({ data: chegadaFormatada, valor: 100 }));
+        localStorage.setItem("saidaReserva", JSON.stringify({ data: saidaFormatada, valor: 100 }));
+        localStorage.setItem("quantidadeHospedes", quantidadeHospedes);
+
+        window.location.href = "pagamento.php";
+      });
+    });
+  </script>
 </html>
