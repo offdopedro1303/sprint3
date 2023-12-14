65f876d946fd7e27aec36e993278a15107f76ae9
Natallyreis <130668435+Natallyreis@users.noreply.github.com>
2023-12-14 08:34:00 -0300

    commit

--- blog.php
+++ blog.php
@@ -37,33 +37,92 @@
 
     <section>
         <article>
-            <h2>Politica de Segurança do AconCheguei</h2>
+            <h2>Politica de Segurança do AconCheguei:</h2>
             <p>Garantimos segurança total dos clientes. Antes dos proprietários colocarem seus imóveis para alugar, nossa equipe faz vistorias nas residências para garantir que não há câmeras escondidas ou cômodos que coloquem a vida pessoal dos nossos clientes em risco.</p>
         </article>
 
         <article>
-            <h2>Título do Artigo 2</h2>
-            <p>Conteúdo do artigo 2...</p>
+            <h2>Destinos Exclusivos para Uma Estadia Inesquecível:</h2>
+            <p>Explore destinos incríveis e descubra acomodações únicas oferecidas pelo AconCheguei.</p>
         </article>
 
         <article>
-            <h2>Título do Artigo 2</h2>
-            <p>Conteúdo do artigo 2...</p>
+            <h2>Como Escolher o Imóvel Perfeito para Sua Viagem:</h2>
+            <p>Guia prático para ajudar os usuários a selecionar a melhor opção de acomodação com base em suas necessidades e preferências.</p>
         </article>
 
         <article>
-            <h2>Título do Artigo 2</h2>
-            <p>Conteúdo do artigo 2...</p>
+            <h2>Novidades e Promoções Exclusivas:</h2>
+            <p>Mantenha os leitores atualizados sobre novas propriedades, ofertas especiais e promoções do AconCheguei.</p>
         </article>
 
         <article>
-            <h2>Título do Artigo 2</h2>
-            <p>Conteúdo do artigo 2...</p>
+            <h2>Checklist de Viagem: O Que Não Pode Faltar na Sua Mala:</h2>
+            <p>Roupas:
+
+1-Roupas íntimas
+<br>
+2-Camisetas/Blusas
+<br>
+3-Calças/Shorts/Saias
+<br>
+4-Casacos/Blusas de frio (dependendo do clima)
+<br>
+5-Pijamas
+<br>
+6-Roupa de banho (se aplicável)
+<br>
+<br>
+Calçados:
+<br>
+7-Sapatos confortáveis para caminhar
+<br>
+8-Chinelos/Sandálias
+<br>
+9-Sapatos de acordo com a ocasião (ex: sapatos formais, tênis para atividades esportivas)
+<br>
+</p>
         </article>
 
         <article>
-            <h2>Título do Artigo 2</h2>
-            <p>Conteúdo do artigo 2...</p>
+            <h2>Sustentabilidade em Viagens: Dicas para uma Estadia Eco-friendly:</h2>
+            <p>Promover a sustentabilidade em viagens é uma prática importante para minimizar o impacto ambiental e contribuir para um turismo mais responsável. Aqui estão algumas dicas para uma estadia eco-friendly:
+                <br>
+                <br>
+
+Escolha Acomodações Sustentáveis:
+<br>
+Opte por hotéis, pousadas ou aluguéis de férias que possuam certificações ambientais ou que adotem práticas sustentáveis, como reciclagem, uso eficiente de energia e gestão responsável de recursos hídricos.
+<br>
+<br>
+Reduza o Consumo de Energia:
+<br>
+Desligue luzes, aparelhos eletrônicos e ar-condicionado ao sair do quarto.
+Utilize cortinas para aproveitar a luz natural durante o dia.
+<br>
+<br>
+Economize Água:
+<br>
+Tome banhos rápidos.
+<br>
+Reutilize toalhas e lençóis quando possível.
+<br>
+Relate vazamentos ou problemas com encanamentos.
+<br>
+<br>
+Minimize o Desperdício de Plástico:
+<br>
+Leve uma garrafa de água reutilizável.
+<br>
+Evite produtos descartáveis, como talheres e copos.
+<br>
+Recuse sacolas plásticas e traga sua própria bolsa reutilizável.
+<br>
+<br>
+Utilize Transporte Sustentável:
+<br>
+Opte por meios de transporte mais ecológicos, como bicicletas ou transporte público.
+Se possível, escolha voos diretos para reduzir as emissões de carbono.</p>
         </article>
     </section>
 
